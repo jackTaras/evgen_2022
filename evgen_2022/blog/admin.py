c831412d068dee8940000fc0085e781fe05de858
jackTaras <bizimec@gmail.com>
2022-05-11 16:14:21 +0300

    Initial commit

---
+++ evgen_2022/blog/admin.py
@@ -0,0 +1,10 @@
+
+# Register your models here.
+
+from django.contrib import admin
+from . models import Post,Category
+
+# Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Category)
